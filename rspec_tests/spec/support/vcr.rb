1bdf6c2f71ee896c18a5aaf352cb8375cd551fb4
Eder Roger Souza <eder.roger.souza@gmail.com>
2015-03-29 18:17:05 -0300

    ajustments

--- rspec_tests/spec/support/vcr.rb
+++ rspec_tests/spec/support/vcr.rb
@@ -2,4 +2,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.filter_sensitive_data('<MOIP_KEY>'){'my_key'}
+  c.filter_sensitive_data('<MOIP_TOKEN>'){'my_token'}
 end
